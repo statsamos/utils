540411db2d3182f6210784436f0b512278f7a8e1
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2023-05-03 14:01:48 -0600

    initial

---
+++ createNewBranchVersionGhCLI.ps1
@@ -0,0 +1,24 @@
+﻿$myhome = "C:\Users\squar\source\statsamos"
+$ignoredirs = @("utils", "skipthis")
+$oldbranch = "28"
+$newbranch = "29"
+
+Get-ChildItem -Path $myhome -Directory | ForEach-Object {
+    $directory = $_.FullName
+    if ($ignoredirs -contains $_.Name) {
+        Write-Host "Skipping directory: $directory"
+        return
+    }
+    
+    Write-Host "Processing directory: $directory"
+    Set-Location $directory
+    
+    git checkout $oldbranch
+    git checkout -b $newbranch
+
+    # Push the new branch to GitHub
+    git push --set-upstream origin $newbranch
+
+    # Set the new branch as the default branch in GitHub
+    gh api -X PATCH /repos/:owner/:repo --field default_branch=$newbranch
+}
---
+++ updateBranches.ps1
@@ -0,0 +1,20 @@
+﻿$myhome = "C:\Users\squar\source\statsamos"
+$ignoredirs = @("utils", "skipthis")
+$oldbranch = "master"
+$newbranch = "28"
+
+Get-ChildItem -Path $myhome -Directory | ForEach-Object {
+    $directory = $_.FullName
+    if ($ignoredirs -contains $_.Name) {
+        Write-Host "Skipping directory: $directory"
+        return
+    }
+    
+    Write-Host "Processing directory: $directory"
+    Set-Location $directory
+    
+    git branch -m $oldbranch $newbranch
+    git fetch
+    git branch -u origin/$newbranch $newbranch
+    git remote set-head origin -a
+}
